5dd22e062e9c5efbdbc23c100c89b907a0250b3b
Katie Power <Powerk8@tcd.ie>
2024-11-04 18:13:50 +0000

    Add basic nested route rendering logic

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,12 @@ function App() {
         <Route path="/pricing" element={<Pricing />}></Route>
         <Route path="/product" element={<Product />}></Route>
         <Route path="/login" element={<Login />}></Route>
-        <Route path="/app" element={<AppLayout />}></Route>
+        <Route path="/app" element={<AppLayout />}>
+          <Route index element={<p>List of cities</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
     </div>
